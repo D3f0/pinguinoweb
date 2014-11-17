ba1e394905336d8c84889310e2fc359e318c40ec
Nahuel <nahuel.defosse@gmail.com>
2014-11-16 22:46:56 -0300

    Cliente y servidor

--- con_brython.py
+++ con_brython.py
@@ -2,12 +2,14 @@
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 from flask import Flask, render_template, request
-from gevent import sleep
+from gevent import sleep, spawn_later
 from datetime import datetime
-# from pynguino import PinguinoProcessing
-# import webbrowser
-# import thread
-# import time
+import json
+import random, os
+import webbrowser
+
+random.seed(os.getpid())
+
 
 app = Flask(__name__)
 
@@ -22,11 +24,19 @@ def websocket():
     if request.environ.get('wsgi.websocket'):
         ws = request.environ['wsgi.websocket']
         while True:
-            sleep(1)
-            ws.send("%s" % datetime.now())
+            sleep(0.5)
+            ahora = "%s" % datetime.now()
+            dato = {
+                'fecha': ahora,
+                'led': random.randint(0, 8)
+            }
+            cadena = json.dumps(dato)
+            ws.send(cadena)
     return
 
 if __name__ == '__main__':
-    print "Lanzando servidor"
+    print u"Lanzando servidor y un navegador 2 segundos más tarde"
+
     http_server = WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    spawn_later(2, lambda : webbrowser.open('http://localhost:5000'))
     http_server.serve_forever()
